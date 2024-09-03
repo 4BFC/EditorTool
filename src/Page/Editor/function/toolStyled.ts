745b8358f80acee4070270f05a3c323ab24d22fa
4Bee <4bee.code@gmail.com>
2024-09-03 16:17:22 +0900

    🚩: 스타일 적용이 해제가 되는 함수를 위한 틀 구현

--- src/Page/Editor/function/toolStyled.ts
+++ src/Page/Editor/function/toolStyled.ts
@@ -1,12 +1,35 @@
 
 enum ToolType {
+  none,
   underLineIcon,
   boldIcon,
   italicIcon,
   lineThroughIcon,
 }
-
+//enum으로 반환된 key값으로 적용
 export const toolStyled = (style: any) => {
+
+  const selection = window.getSelection();
+  if (selection && selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    const span = document.createElement("span");
+    if (style === "bold") {
+      span.style.fontWeight = "bold";
+    }
+    if (style === "italic") {
+      span.style.fontStyle = "italic";
+    }
+    if (style === "underline") {
+      span.style.textDecoration = "underline";
+    }
+    if (style === "strikeThrough") {
+      span.style.textDecoration = "line-through";
+    }
+    range.surroundContents(span);
+  }
+};
+
+export const noneStyled = (style: any) => {
 
   const selection = window.getSelection();
   if (selection && selection.rangeCount > 0) {
